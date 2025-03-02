e3490be99f4416bd04cd72905868b789090615e4
zhumingtao <241250048@smail.nju.edu.cn>
2025-03-02 11:32:32 +0800

    aa

--- lec1/Doglauncher.java
+++ lec1/Doglauncher.java
@@ -3,6 +3,7 @@ package lec1;
 public class Doglauncher {
     public static void main(String[] args) {
         /**a class uses another class is called client */
+        /**java要包和目录一致，编译时要在父目录编译运行，因为在本目录下找不到包 */
         Dog d = new Dog(20);
         d.makeNoise();
     }
