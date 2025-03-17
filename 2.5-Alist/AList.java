cbf6385736b830cd8fa2de9c1809f23d8edee52f
zhumingtaoNJ <2830849787@qq.com>
2025-03-17 09:56:45 +0800

    Update AList.java

--- 2.5-Alist/AList.java
+++ 2.5-Alist/AList.java
@@ -2,31 +2,38 @@
  */
 
 public class AList {
+    private int size;
+    private int[] items;
     /** Creates an empty list. */
     public AList() {
+        size = 0;
+        items = new int[100];
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        items[size] = x;
+        size += 1;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return item[size - 1];
     }
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return item[i];
     }
 
     /** Returns the number of items in the list. */
     public int size() {
-        return 0;        
+        return size;
     }
 
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+        items[size - 1] = 0;
+        size -= 1;
     }
 } 
